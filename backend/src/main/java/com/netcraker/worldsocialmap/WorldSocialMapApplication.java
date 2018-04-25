e29b4053467120858b969289f7beb04dbc0c1eb0
Anrymart <a.mart@list.ru>
2018-04-25 22:50:15 +0300

    Pom versions update, application properties update.

--- backend/src/main/java/com/netcraker/worldsocialmap/WorldSocialMapApplication.java
+++ backend/src/main/java/com/netcraker/worldsocialmap/WorldSocialMapApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtDemoApplication {
+public class WorldSocialMapApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JwtDemoApplication.class, args);
+        SpringApplication.run(WorldSocialMapApplication.class, args);
     }
 }
